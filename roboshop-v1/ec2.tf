ea567530807e0efc7d734fc9b0cc569067d6ecf8
raiyan <raiyan.m1201@gmail.com>
2024-07-16 23:49:51 -0400

    Terraform Code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -7,3 +7,23 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+resource "aws_instance" "mongo" {
+  ami           = "ami-041e2ea9402c46c32" # us-west-2
+  instance_type = "t2.small"
+  vpc_security_group_ids = ["sg-0eb0ffe1f55b4d12b"]
+
+  tags = {
+    Name = "mongo"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-041e2ea9402c46c32" # us-west-2
+  instance_type = "t2.small"
+  vpc_security_group_ids = ["sg-0eb0ffe1f55b4d12b"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
