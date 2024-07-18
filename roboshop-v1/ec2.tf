d025d73908bea2d7cbe7b845fc3eab57e53d2114
raiyan <raiyan.m1201@gmail.com>
2024-07-18 03:45:29 -0400

    Terraform Code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -15,8 +15,7 @@ resource "aws_route53_record" "frontend" {
   ttl     = 15
   records = [aws_instance.frontend.private_ip]
 }
-
-resource "aws_instance" "mongo" {
+ource "aws_instance" "mongo" {
   ami           = "ami-041e2ea9402c46c32" # us-west-2
   instance_type = "t2.small"
   vpc_security_group_ids = ["sg-0eb0ffe1f55b4d12b"]
