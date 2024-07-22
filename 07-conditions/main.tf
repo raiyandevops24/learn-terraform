34d76dddad0dd916f9f80151dcfa104cd537dab4
raiyan <raiyan.m1201@gmail.com>
2024-07-22 04:21:44 -0400

    Terraform Code

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "frontend" {
   zone_id = "Z0994447ZVXCLP0MPPND"
   name    = "frontend.dev.raiyan-m.online"
   type    = "A"
-  ttl     = var.ttl
+  ttl     = var.ttl == "" ? 15 : var.ttl
   records = ["1.1.1.1"]
 }
 
