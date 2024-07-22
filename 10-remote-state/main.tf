90b013450cdd8845e7a9c3877de1f373c63d5363
raiyanmuntasir <raiyan.m1201@gmail.com>
2024-07-22 18:02:40 -0400

    Terraform Code

--- 10-remote-state/main.tf
+++ 10-remote-state/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "test" {}
 terraform {
   backend "s3" {
     bucket = "raiyan-terraform"
-    key    = "test/terraform.tfstate"
+    key    = "test1/terraform.tfstate"
     region = "us-east-1"
   }
 }
