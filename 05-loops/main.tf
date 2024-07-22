17e10deec9ab9d8759d246af6c88bdab11730ffd
raiyan <raiyan.m1201@gmail.com>
2024-07-22 02:17:00 -0400

    Terraform Code

--- 05-loops/main.tf
+++ 05-loops/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "test" {
   count = 3
 }
+
+variable "components" {
+  default = ["frontend", "mongo", "catalogue"]
+}
+
+resource "null_resource" "tst1" {
+  count= length(var.components)
+}
