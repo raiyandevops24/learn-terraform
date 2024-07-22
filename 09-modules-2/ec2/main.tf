27533a6cf26766f91fc9acd9dffe2f3839b496c0
raiyanmuntasir <raiyan.m1201@gmail.com>
2024-07-22 15:13:20 -0400

    Terraform Code

--- 09-modules-2/ec2/main.tf
+++ 09-modules-2/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami           = "ami-041e2ea9402c46c32" # us-west-2
   instance_type = "t3.small"
-  vpc_security_group_ids = "sg-0eb0ffe1f55b4d12b"
+  vpc_security_group_ids = ["sg-0eb0ffe1f55b4d12b"]
 
   tags = {
     Name = "test"
